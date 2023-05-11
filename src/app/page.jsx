efe0469c84a896c77f5ba79f6de9f752ba7dba61
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-05-11 23:19:01 +0400

    add home header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
 export default function page() {
-  return <div>Home</div>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
