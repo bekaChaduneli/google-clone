335590c7b783cbeb3500f7afc7be99f1423c0718
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-05-12 19:59:15 +0400

    create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,9 @@
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -9,7 +11,6 @@ export default async function WebSearchPage({ searchParams }) {
     throw new Error("Something went wrong");
   }
   const data = await response.json();
-  console.log(data);
   const results = data.items;
   if (!results) {
     return (
@@ -24,5 +25,5 @@ export default async function WebSearchPage({ searchParams }) {
       </div>
     );
   }
-  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
+  return <>{results && <WebSearchResults results={data} />}</>;
 }
