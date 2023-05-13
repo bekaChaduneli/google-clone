2e3acfad15d6b4445bb31115f9de408a41f9798b
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-05-13 19:48:18 +0400

    Remove delay on search

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import React from "react";
 
 export default async function ImageSearchPage({ searchParams }) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   const startIndex = searchParams.start || "1";
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}&searchType=image&start=${startIndex}`
